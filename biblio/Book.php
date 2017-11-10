682a48aea8084423602998af56d48c2dee608479
frpele <frpele@gmail.com>
2017-11-11 00:28:25 +0100

    TP3

--- biblio/Book.php
+++ biblio/Book.php
@@ -1,13 +1,15 @@
 <?php
 class Book {
+  private $db = NULL;
   private $id = NULL;
   private $title = NULL;
   private $isbn = NULL;
   private $pages = NULL;
   private $id_author = NULL;
 
-  public function __construct($id, $title, $isbn,$pages, $id_author) {
+  public function __construct($db, $id, $title, $isbn,$pages, $id_author) {
     // assignation directe ans setters
+    $this->setDb($db);
     $this->id = $id;
     $this->title = $title;
     $this->isbn = $isbn;
@@ -20,5 +22,40 @@ class Book {
   public function getIsbn() { return $this->isbn;}
   public function getPages() { return $this->pages;}
   public function getIdAuthor() { return $this->id_author;}
+
+  private function setDb(PDO $db) {
+    // on indique de quel type sera l'argument
+    // ce renseignement, qu'on utilise uniquement dans le cas
+    // de types complexes (array, object), est facultatif
+    $this->db = $db;
+    return $this->db;
+  }
+
+  public function list() {
+
+    $query = $this->db->prepare
+    ('SELECT book.title, book.id as id_book,
+      book.id_author, author.lastname
+      FROM author
+      JOIN book ON author.id = book.id_author
+      ORDER BY book.title ASC
+    ');
+
+    // 2 Execution
+    $query->execute();
+    //3 Récupération des données (fetch)
+    $books = $query-> fetchAll(PDO::FETCH_OBJ);
+
+    return $books;
+
+  }
+
+
+
+
+
 }
+
+
+
 ?>
--- biblio/book/list.php
+++ biblio/book/list.php
@@ -1,4 +1,36 @@
-<?php  ?>
+<?php
 
+include('./Book.php');
+
+$book = new Book($db, NULL, NULL, NULL, NULL,NULL);
+
+$books = $book->list();// retourne un tableau d'objets books
+ ?>
 
 <h2>Liste des livres</h2>
+
+<table class="table table-bordered table-striped">
+  <tr>
+    <th>ID</th>
+    <th>Titre</th>
+    <th>Auteur</th>
+  </tr>
+<?php $i=0; ?>
+<?php foreach ($books as $book): ?>
+
+<tr>
+  <td><?= ++$i ?></td>
+  <td><?=$book->title ?></td>
+  <td><?=$book->author ?></td>
+  <td>
+    <a
+    href="#"
+    class="btn btn-primary btn-xs">Modifier</a>
+    <a
+    href="#"
+    class="btn btn-danger btn-xs">Supprimer</a>
+
+  </td>
+</tr>
+
+<?php endforeach ?>
