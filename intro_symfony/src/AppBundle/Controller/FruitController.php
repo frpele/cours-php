475c22634da27f7396196eec466f9bf08bfc6d4b
frpele <frpele@gmail.com>
2017-11-15 14:13:21 +0100

    tp

--- intro_symfony/src/AppBundle/Controller/FruitController.php
+++ intro_symfony/src/AppBundle/Controller/FruitController.php
@@ -1,12 +1,10 @@
 <?php
 
-// chemin
 namespace AppBundle\Controller;
 
-//include
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
 * @Route("fruits")
@@ -18,7 +16,22 @@ class FruitController extends Controller {
   * @Route("/", name="fruit_admin_page")
   */
 
-  public function indexAction() {
+  public function indexAction(Request $r) {
+    // var_dump($request);
+$post = $r->request;
+echo $post->get('name');
+echo $r->request->get('origin');
+
+if ($r->getMethod() == 'POST') {
+  $name = $post->get('name');
+  $origin = $post->get('origin');
+  $comestible = $post->get('comestible');
+}
+
+// suite : créer un objet fruit, le transmettre à un manager
+// pour l'enresgistrment en db
+
+// var_dump($post);
     return $this->render('fruit/index.html.twig', array(
 
     ));
