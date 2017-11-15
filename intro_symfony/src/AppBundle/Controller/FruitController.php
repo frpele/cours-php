fc18d307bcddef84b33fed70b7d13c02da7904d5
frpele <frpele@gmail.com>
2017-11-15 16:03:15 +0100

    Symfony

--- intro_symfony/src/AppBundle/Controller/FruitController.php
+++ intro_symfony/src/AppBundle/Controller/FruitController.php
@@ -5,6 +5,8 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Entity\Fruit;
+
 
 /**
 * @Route("fruits")
@@ -16,25 +18,44 @@ class FruitController extends Controller {
   * @Route("/", name="fruit_admin_page")
   */
 
-  public function indexAction(Request $r) {
-    // var_dump($request);
-$post = $r->request;
-echo $post->get('name');
-echo $r->request->get('origin');
+public function indexAction(Request $r) {
+  // var_dump($request);
+  $post = $r->request;
+  echo $post->get('name');
+  echo $r->request->get('origin');
 
-if ($r->getMethod() == 'POST') {
-  $name = $post->get('name');
-  $origin = $post->get('origin');
-  $comestible = $post->get('comestible');
-}
+  if ($r->getMethod() == 'POST') {
+    $name = $post->get('name');
+    $origin = $post->get('origin');
+    $comestible = $post->get('comestible');
 
-// suite : créer un objet fruit, le transmettre à un manager
-// pour l'enresgistrment en db
+    // Vérification du contenu de la variable $comestible
+    if($comestible) {
+      $comestible = 1;
+    } else {
+      $comestible = 0;
+    }
 
-// var_dump($post);
-    return $this->render('fruit/index.html.twig', array(
+    $fruit = new Fruit();
+    // hydratation
+    $fruit->setName($name);
+    $fruit->setOrigin($origin);
+    $fruit->setComestible($comestible);
 
+    // Utilisation du EntityManager
+
+    $em = $this->getDoctrine()->getManager();
+    $em->persist($fruit); // prépare la requete en insertion
+    // mais n'éxecute aucune requete sql
+    $em->flush();
+
+  }
+
+  // suite : créer un objet fruit, le transmettre à un manager
+  // pour l'enresgistrment en db
+
+  // var_dump($post);
+  return $this->render('fruit/index.html.twig', array(
     ));
-
   }
 }
