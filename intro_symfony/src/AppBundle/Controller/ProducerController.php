12c811c80102311366a06905353c9d717de844f5
frpele <frpele@gmail.com>
2017-11-16 16:02:54 +0100

    test

--- intro_symfony/src/AppBundle/Controller/ProducerController.php
+++ intro_symfony/src/AppBundle/Controller/ProducerController.php
@@ -4,6 +4,14 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use AppBundle\Entity\Producer;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * @Route("/producer")
+ */
 
 class ProducerController extends Controller
 {
@@ -20,11 +28,30 @@ class ProducerController extends Controller
     /**
      * @Route("/add")
      */
-    public function addAction()
+    public function addAction(Request $request)
     {
+        // Création d'un objet vide
+        $producer = new Producer();
+        // La méthode createFormBuilder permet de créer un form en pure POO
+        // pas de balise html
+
+        $form =$this->createFormBuilder($producer)
+        ->add('name', TextType::class)
+        ->add('submit', SubmitType::class, array(
+          'label' =>'Enregistrer',
+        ))
+        ->getForm();
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted()) {
+          echo 'POST';
+        }
+
         return $this->render('AppBundle:Producer:add.html.twig', array(
-            // ...
-        ));
+            'form' =>$form->createView()
+
+            ));
     }
 
     /**
